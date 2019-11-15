9cd6586fbc93c4e27285c3fa9cf58626c0292d6c
Dylanbatar <dylanbatar2@gmail.com>
2019-11-14 22:08:50 -0500

    add de los cors

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 
 app.use(require('./route/index'));
 app.set('port', process.env.PORT || 3000);
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 
 app.listen(app.get('port'), () => {
     console.log(`escuchando en el puerto ${app.get('port')}`);
